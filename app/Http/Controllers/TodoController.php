bd84bfe0e33869af7fc633ad0e01a63410aed6b9
AhsanRao <35622245+Ahsanrao237@users.noreply.github.com>
2021-04-17 15:49:18 +0500

    todo controller updated

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -13,31 +13,62 @@ class TodoController extends Controller
         //
     }
 
-    
+
+
     public function create()
     {
-        //
+        return view('add_todo');
     }
 
-    public function store(Request $request)
+
+
+    public function store(Request $req)
     {
-        //
+        $todo = new Todo;
+
+        $todo->name= $req->username;
+        // $student->firstName= $req->input('firstName');
+
+        $todo->save();
+
+        $req->session()->flash('msg',"data submitted");
+        $req->session()->flash('class',"succ");
+
+        return redirect('todo_show');
     }
+
+
 
     public function show(Todo $todo)
     {
         return view("todo_view")->with("todo_arr",Todo::all());
     }
 
-    public function edit(Todo $todo)
+
+
+    public function edit(Todo $todo, $id)
     {
-        //
+        return view("edit_todo")->with("todo_arr",Todo::find($id));
     }
+
+
 
     public function update(Request $request, Todo $todo)
     {
-        //
+        $data= Todo::find($request->id);
+
+        $data->name= $request->username;
+        // $student->firstName= $req->input('firstName');
+
+        $data->save();
+
+        $request->session()->flash('msg',"data updated");
+        $request->session()->flash('class',"succ");
+
+        return redirect('todo_show');
     }
+
+
 
     public function destroy(Todo $todo, $id)
     {
